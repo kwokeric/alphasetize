9d3b3c80fbc1a813f0882b5bb47663a371da7a2e
Eric Kwok <ericc.kwok@gmail.com>
2019-04-12 00:20:50 -0700

    feat: add header skeleton

---
+++ src/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        Alphaset
+      </div>
+    );
+  }
+}
+
+export default Header;
