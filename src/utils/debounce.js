acb90ce059efa7251d4b7a3ca93d15a04fd1fd58
Eric Kwok <erickw@zillowgroup.com>
2019-04-28 16:21:33 -0700

    feat: implement debounce

---
+++ src/utils/debounce.js
@@ -0,0 +1,11 @@
+const debounce = (fn, t) => {
+    let timeout;
+
+    return () => {
+        clearTimeout(timeout);
+
+        timeout = setTimeout(fn(), t);
+    };
+};
+
+export default debounce;
