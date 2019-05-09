e1498793ad1b57d7f7c11d4b3d8f05ae5e3bc4d0
Eric Kwok <erickw@zillowgroup.com>
2019-05-09 11:33:10 -0700

    feat: implement ket matching fn

--- src/components/List/controller.js
+++ src/components/List/controller.js
@@ -1,6 +1,38 @@
 const controller = {
-    getMatches(item, list) {
-        // return matching songs
+    getMatches(index, list) {
+        const currItem = list[index];
+        const currCamKey = currItem.camKey;
+        const currCamNum = Number(currCamKey.slice(0, currCamKey.length - 1));
+        const stepUp =
+            (currCamNum + 1 === 13 ? 1 : currCamNum + 1) +
+            currCamKey[currCamKey.length - 1];
+        const stepDown =
+            (currCamNum - 1 === 0 ? 12 : currCamNum - 1) +
+            currCamKey[currCamKey.length - 1];
+        const stepSide =
+            currCamNum +
+            (currCamKey[currCamKey.length - 1] === 'A' ? 'B' : 'A');
+        console.log('stepUp', stepUp);
+        console.log('stepDown', stepDown);
+        console.log('stepSide', stepSide);
+
+        let perfectMatches = []; // same key
+        let keyMatches = []; // key change
+        let modeMatches = []; // mode change
+
+        list.forEach((track, idx) => {
+            if (idx === index) {
+                // do nothing
+            } else if (track.camKey === currCamKey) {
+                perfectMatches.push(idx);
+            } else if (track.camKey === stepUp || track.camKey === stepDown) {
+                keyMatches.push(idx);
+            } else if (track.camKey === stepSide) {
+                modeMatches.push(idx);
+            }
+        });
+
+        return { perfectMatches, keyMatches, modeMatches };
     }
 };
 
