b7a1f2bc0aea7ffae7a10a9f3e7ffb006edd4703
Eric Kwok <erickw@zillowgroup.com>
2019-08-20 00:11:30 -0700

    feat: mweb playlist import button

---
+++ src/components/PlaylistImport/index.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import map from 'lodash/map';
+
+import PlaylistActions from '../../redux/actions/PlaylistActions';
+import './style.css';
+import cx from '../../utils/cx.js';
+
+const PLAYLIST_MAX_LENGTH = 100;
+
+class PlaylistImport extends Component {
+    static defaultProps = {
+        playlists: []
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeIndex: -1,
+            showError: false
+        };
+    }
+
+    handleClick = i => {
+        this.setState({ activeIndex: i });
+    };
+
+    handleImport = (i, shouldDisable) => {
+        const { dispatch, playlists, onHideModal } = this.props;
+        const { activeIndex } = this.state;
+        const currPlaylist = playlists[activeIndex];
+
+        if (activeIndex < 0) {
+            return;
+        }
+
+        if (currPlaylist.length > 100) {
+            this.setState({ showError: true });
+            setTimeout(() => this.setState({ showError: false }), 4000);
+            return;
+        }
+
+        dispatch(PlaylistActions.getAndSetPlaylist(currPlaylist.id)).catch(
+            err => console.log(err)
+        );
+
+        onHideModal();
+    };
+
+    handleCancel = () => {
+        const { onHideModal } = this.props;
+        onHideModal();
+    };
+
+    renderPlaylist = (playlist, idx) => {
+        const { activeIndex } = this.state;
+        const shouldDisable = playlist.length > PLAYLIST_MAX_LENGTH;
+
+        return (
+            <div
+                className={cx('PlaylistImport-playlist noselect', {
+                    'PlaylistImport-playlist-disable': shouldDisable,
+                    'PlaylistImport-playlist-active': activeIndex === idx
+                })}
+                key={playlist.id + idx}
+                onClick={() => this.handleClick(idx, shouldDisable)}
+            >
+                <div>{playlist.name}</div>
+                <div className="PlaylistImport-playlist-length">
+                    {playlist.length} songs
+                </div>
+            </div>
+        );
+    };
+
+    render() {
+        const { playlists } = this.props;
+        const { showError, activeIndex } = this.state;
+
+        return (
+            <div className="PlaylistImport">
+                <h1 className="PlaylistImport-header">YOUR PLAYLISTS</h1>
+                <div className="PlaylistImport-container">
+                    {map(playlists, (p, i) => this.renderPlaylist(p, i))}
+                </div>
+                <div className="PlaylistImport-bottom">
+                    <div className="PlaylistImport-error-container">
+                        <div
+                            className={cx('PlaylistImport-error', {
+                                'PlaylistImport-error-hidden': !showError
+                            })}
+                        >
+                            Please select a playlist with less than{' '}
+                            {PLAYLIST_MAX_LENGTH} songs
+                        </div>
+                    </div>
+                    <div className="PlaylistImport-btn-container">
+                        <div
+                            className={cx('PlaylistImport-btn', {
+                                'PlaylistImport-btn-active': activeIndex !== -1
+                            })}
+                            onClick={this.handleImport}
+                        >
+                            IMPORT
+                        </div>
+                        <div
+                            className="PlaylistImport-btn-cancel"
+                            onClick={this.handleCancel}
+                        >
+                            CANCEL
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect()(PlaylistImport);
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,8 +3,10 @@ import { connect } from 'react-redux';
 
 import PlaylistActions from '../../redux/actions/PlaylistActions';
 import Autocomplete from '../Autocomplete';
-import PlaylistModal from '../PlaylistModal';
+import PlaylistImport from '../PlaylistImport';
+import Modal from '../Modal';
 import List from '../List';
+import cx from '../../utils/cx.js';
 import './style.css';
 
 class Search extends Component {
@@ -12,7 +14,8 @@ class Search extends Component {
         super(props);
 
         this.state = {
-            showPlaylistModal: false
+            showPlaylistImport: false,
+            fullCta: null
         };
     }
 
@@ -27,7 +30,7 @@ class Search extends Component {
             .then(playlists => {
                 return dispatch(PlaylistActions.setPlaylists(playlists.items));
             })
-            .then(() => this.setState({ showPlaylistModal: true }))
+            .then(() => this.setState({ showPlaylistImport: true }))
             .catch(err => console.log(err));
     };
 
@@ -38,40 +41,73 @@ class Search extends Component {
     };
 
     handleHideModal = () => {
-        this.setState({ showPlaylistModal: false });
+        this.setState({ showPlaylistImport: false });
+    };
+
+    handleImportMobile = () => {
+        this.setState({ fullCta: 'playlist' });
+        this.handleImport();
+    };
+
+    handleCancelImportMobile = () => {
+        this.setState({ fullCta: '', showPlaylistImport: false });
+    };
+
+    renderMobileView = () => {
+        return (
+            <div className="Search-top-m">
+                <div className="Search-autocomplete-m">
+                    <Autocomplete />
+                </div>
+
+                <div
+                    className="Search-playlist-m"
+                    onClick={this.handleImportMobile}
+                >
+                    <div className="Search-playlist-open">+</div>
+                </div>
+            </div>
+        );
     };
 
     render() {
         const { list, playlists } = this.props;
-        const { showPlaylistModal } = this.state;
+        const { showPlaylistImport } = this.state;
+        const isMobile = true;
 
         return (
             <div className="Search">
-                {showPlaylistModal && (
-                    <PlaylistModal
-                        playlists={playlists}
-                        onHideModal={this.handleHideModal}
-                    />
+                {showPlaylistImport && (
+                    <Modal onHideModal={this.handleHideModal}>
+                        <PlaylistImport
+                            playlists={playlists}
+                            onHideModal={this.handleHideModal}
+                        />
+                    </Modal>
                 )}
-                <div className="Search-top">
-                    <div className="Search-autocomplete">
-                        <Autocomplete />
-                    </div>
-                    <div className="Search-import-export">
-                        <div
-                            className="Search-import noselect"
-                            onClick={this.handleImport}
-                        >
-                            IMPORT PLAYLIST
+                {isMobile ? (
+                    this.renderMobileView()
+                ) : (
+                    <div className="Search-top">
+                        <div className="Search-autocomplete">
+                            <Autocomplete />
                         </div>
-                        <div
-                            className="Search-export noselect"
-                            onClick={this.handleExport}
-                        >
-                            EXPORT PLAYLIST
+                        <div className="Search-import-export">
+                            <div
+                                className="Search-import noselect"
+                                onClick={this.handleImport}
+                            >
+                                IMPORT PLAYLIST
+                            </div>
+                            <div
+                                className="Search-export noselect"
+                                onClick={this.handleExport}
+                            >
+                                EXPORT PLAYLIST
+                            </div>
                         </div>
                     </div>
-                </div>
+                )}
                 <List list={list} />
             </div>
         );
