11aead6a44c184037e372a99b21b620b064824e6
Eric Kwok <erickw@zillowgroup.com>
2019-09-18 14:56:35 -0700

    feat: detect hour of mouse position

--- src/components/About/controller.js
+++ src/components/About/controller.js
@@ -1,5 +1,5 @@
 const controller = {
-    isMouseInsideImage({ mouse, img }) {
+    isMouseInsideCircle({ mouse, img }) {
         const imgRadius = img.clientHeight / 2;
         const imgCenterTop = img.offsetTop + imgRadius;
         const imgCenterLeft = img.offsetLeft + imgRadius;
@@ -12,6 +12,54 @@ const controller = {
         );
 
         return distanceMouseFromImgCenter <= imgRadius;
+    },
+    getHourPosition({ mouse, img }) {
+        const imgRadius = img.clientHeight / 2;
+        const imgCenterY = img.offsetTop + imgRadius;
+        const imgCenterX = img.offsetLeft + imgRadius;
+
+        const mouseY = mouse.pageY;
+        const mouseX = mouse.pageX;
+
+        const angleFromNegativeX =
+            (Math.atan2(imgCenterY - mouseY, imgCenterX - mouseX) * 180) /
+            Math.PI;
+        const threeSixtyFromNegativeX =
+            angleFromNegativeX > 0
+                ? angleFromNegativeX
+                : 360 + angleFromNegativeX;
+        const angleFromTop =
+            threeSixtyFromNegativeX - 90 >= 0
+                ? threeSixtyFromNegativeX - 90
+                : 270 + threeSixtyFromNegativeX;
+
+        if (angleFromTop - 15 < 0) {
+            return 12;
+        } else if (angleFromTop - 15 < 30) {
+            return 1;
+        } else if (angleFromTop - 15 < 60) {
+            return 2;
+        } else if (angleFromTop - 15 < 90) {
+            return 3;
+        } else if (angleFromTop - 15 < 120) {
+            return 4;
+        } else if (angleFromTop - 15 < 150) {
+            return 5;
+        } else if (angleFromTop - 15 < 180) {
+            return 6;
+        } else if (angleFromTop - 15 < 210) {
+            return 7;
+        } else if (angleFromTop - 15 < 240) {
+            return 8;
+        } else if (angleFromTop - 15 < 270) {
+            return 9;
+        } else if (angleFromTop - 15 < 300) {
+            return 10;
+        } else if (angleFromTop - 15 < 330) {
+            return 11;
+        } else {
+            return 12;
+        }
     }
 };
 
