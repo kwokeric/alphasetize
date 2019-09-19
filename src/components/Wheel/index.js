1e7788b2bcc37119c32b232b058b660a2764a552
Eric Kwok <erickw@zillowgroup.com>
2019-09-19 00:17:24 -0700

    feat: implement correct shading of matching keys

--- src/components/Wheel/index.js
+++ src/components/Wheel/index.js
@@ -51,7 +51,7 @@ class Wheel extends Component {
         } else {
             if (hoverSection.hour || hoverSection.ring) {
                 this.setState({
-                    hoverSection: {}
+                    // hoverSection: {}
                 });
             }
         }
@@ -84,12 +84,47 @@ class Wheel extends Component {
 
     renderWheelCover = () => {
         const { hoverSection } = this.state;
-        console.log(hoverSection);
+        const { hour, ring, rotation } = hoverSection;
+
+        if (!hour || !ring) {
+            return null;
+        }
+
+        const innerStyle = {
+            transform: `rotate(${rotation - (ring === 'A' ? 45 : 75)}deg)`
+        };
+        const outerStyle = {
+            transform: `rotate(${rotation - (ring === 'B' ? 45 : 75)}deg)`
+        };
 
         return (
             <Fragment>
-                <div className="Wheel-image-inner-cover" />
-                <div className="Wheel-image-outer-cover" />
+                <div className="Wheel-image-eye-cover">hi</div>
+                <div className="Wheel-image-inner-cover" style={innerStyle}>
+                    <svg className="Wheel-svg-inner" height="210" width="210">
+                        <circle
+                            cx="105"
+                            cy="105"
+                            r="75"
+                            opacity="0.8"
+                            strokeWidth="60.5"
+                            strokeDasharray={ring === 'A' ? '353' : '431'}
+                        />
+                    </svg>
+                </div>
+                <div className="Wheel-image-outer-cover" style={outerStyle}>
+                    <svg className="Wheel-image-outer" height="300" width="300">
+                        <circle
+                            className="Wheel-svg-outer"
+                            cx="150"
+                            cy="150"
+                            r="127"
+                            opacity="0.8"
+                            strokeWidth="44"
+                            strokeDasharray={ring === 'B' ? '598' : '731'}
+                        />
+                    </svg>
+                </div>
             </Fragment>
         );
     };
