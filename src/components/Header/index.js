7254d41799ccc122b42ef3680b143e626762d446
erickw <erickw@hotpads.com>
2020-04-24 15:11:20 -0700

    feat: add max-width and styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,34 +11,36 @@ class Header extends Component {
 
         return (
             <div className="Header">
-                <div className="Header-logo">Alphasetize</div>
-                <div className="Header-tabs">
-                    <div
-                        className={
-                            pathname === '/about' || pathname === '/'
-                                ? 'Header-tab-active'
-                                : 'Header-tab'
-                        }
-                    >
-                        <Link to={'/'}>ABOUT</Link>
-                    </div>
-                    <div
-                        className={
-                            pathname === '/create'
-                                ? 'Header-tab-active'
-                                : 'Header-tab'
-                        }
-                    >
-                        <Link to={'/create'}>CREATE</Link>
-                    </div>
-                    <div
-                        className={
-                            pathname === '/contact'
-                                ? 'Header-tab-active'
-                                : 'Header-tab'
-                        }
-                    >
-                        <Link to={'/contact'}>CONTACT</Link>
+                <div className="Header-cont">
+                    <div className="Header-logo">Alphasetize</div>
+                    <div className="Header-tabs">
+                        <div
+                            className={
+                                pathname === '/about' || pathname === '/'
+                                    ? 'Header-tab-active'
+                                    : 'Header-tab'
+                            }
+                        >
+                            <Link to={'/'}>ABOUT</Link>
+                        </div>
+                        <div
+                            className={
+                                pathname === '/create'
+                                    ? 'Header-tab-active'
+                                    : 'Header-tab'
+                            }
+                        >
+                            <Link to={'/create'}>CREATE</Link>
+                        </div>
+                        <div
+                            className={
+                                pathname === '/contact'
+                                    ? 'Header-tab-active'
+                                    : 'Header-tab'
+                            }
+                        >
+                            <Link to={'/contact'}>CONTACT</Link>
+                        </div>
                     </div>
                 </div>
             </div>
