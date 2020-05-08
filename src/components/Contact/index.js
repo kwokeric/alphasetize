e1746f6fb70042ef214954434179251673237aed
erickw <erickw@hotpads.com>
2020-05-08 15:56:42 -0700

    feat: contact styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -23,74 +23,93 @@ class Contact extends Component {
                     <div className="Contact-section">
                         <div className="Contact-description">
                             <div className="Contact-description-tile">
-                                <img
-                                    className="Contact-icon"
-                                    src={IconChat}
-                                    alt="icon-chat"
-                                />
-                                <div className="Contact-subheader">
-                                    Reach out
+                                <div className="Contact-description-top">
+                                    <img
+                                        className="Contact-icon"
+                                        src={IconChat}
+                                        alt="icon-chat"
+                                    />
+                                    <div className="Contact-subheader">
+                                        Reach out
+                                    </div>
                                 </div>
-                                <a
-                                    className="Contact-link"
-                                    href="mailto: erickwokdev@gmail.com"
-                                >
-                                    Email
-                                </a>
+                                <div className="Contact-link">
+                                    <a
+                                        className="link"
+                                        href="mailto: erickwokdev@gmail.com"
+                                    >
+                                        Email
+                                    </a>
+                                </div>
                                 <br />
-                                <a
-                                    className="Contact-link"
-                                    href="https://www.linkedin.com/in/eric-kwok/"
-                                >
-                                    Linked In
-                                </a>
+                                <div className="Contact-link">
+                                    <a
+                                        className="link"
+                                        href="https://www.linkedin.com/in/eric-kwok/"
+                                    >
+                                        Linked In
+                                    </a>
+                                </div>
+
                                 <br />
                             </div>
                             <div className="Contact-description-tile">
-                                <img
-                                    className="Contact-icon"
-                                    src={IconCode}
-                                    alt="icon-code"
-                                />
-                                <div className="Contact-subheader">
-                                    Engineering
+                                <div className="Contact-description-top">
+                                    <img
+                                        className="Contact-icon"
+                                        src={IconMusic}
+                                        alt="icon-bulb"
+                                    />
+                                    <div className="Contact-subheader">
+                                        Music
+                                    </div>
                                 </div>
-                                <a
-                                    className="Contact-link"
-                                    href="https://github.com/kwokeric"
-                                >
-                                    GitHub
-                                </a>
+                                <div className="Contact-link">
+                                    <a
+                                        className="link"
+                                        href="https://soundcloud.com/eks-official"
+                                    >
+                                        SoundCloud
+                                    </a>
+                                </div>
                                 <br />
-                                <a
-                                    className="Contact-link"
-                                    href="https://erickwok.io/"
-                                >
-                                    erickwok.io
-                                </a>
-                                <br />
+                                <div className="Contact-link">
+                                    <a
+                                        className="link"
+                                        href="https://open.spotify.com/user/xtz2r9shizlr37boi82pz14nt?si=vu8A27sYRhq_vukW0q19BQ"
+                                    >
+                                        Spotify
+                                    </a>
+                                </div>
                             </div>
                             <div className="Contact-description-tile">
-                                <img
-                                    className="Contact-icon"
-                                    src={IconMusic}
-                                    alt="icon-bulb"
-                                />
-                                <div className="Contact-subheader">Music</div>
-                                <a
-                                    className="Contact-link"
-                                    href="https://soundcloud.com/eks-official"
-                                >
-                                    SoundCloud
-                                </a>
+                                <div className="Contact-description-top">
+                                    <img
+                                        className="Contact-icon"
+                                        src={IconCode}
+                                        alt="icon-code"
+                                    />
+                                    <div className="Contact-subheader">
+                                        Engineering
+                                    </div>
+                                </div>
+                                <div className="Contact-link">
+                                    <a
+                                        className="link"
+                                        href="https://github.com/kwokeric"
+                                    >
+                                        GitHub
+                                    </a>
+                                </div>
                                 <br />
-                                <a
-                                    className="Contact-link"
-                                    href="https://open.spotify.com/user/xtz2r9shizlr37boi82pz14nt?si=vu8A27sYRhq_vukW0q19BQ"
-                                >
-                                    Spotify
-                                </a>
-                                <br />
+                                <div className="Contact-link">
+                                    <a
+                                        className="link"
+                                        href="https://erickwok.io/"
+                                    >
+                                        erickwok.io
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
